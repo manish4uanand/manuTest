378168e397c564892eb8a2696a02978458efeb50
Bitla13 <manish.anand@bitlasoft.com>
2013-01-28 11:46:29 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ManuTest::Application.config.secret_token = 'efc2b24b12a4352670758c2afc8ea701140f05888a958cd739705380fb3d9012ffce575782894502dd9b3c97df5022e2548b1f023817b6480f8a0c542f8a8925'
